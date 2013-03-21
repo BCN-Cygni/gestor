e75760f9507d96bbb3a1e2c300ee308e8b06e8e2
Joaquín Nader <joaquinnader@gmail.com>
2013-03-21 19:50:00 -0300

    nombre carpeta scrip a script y cambio config

--- config/config.php
+++ config/config.php
@@ -5,11 +5,11 @@ define("HOME", 'http://localhost/gestor/');
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "aulavirtual");
+define("DB_NAME", "gestor");
 
 define("CIFRADO", MCRYPT_RIJNDAEL_256);
 define("MODO", MCRYPT_MODE_ECB);
 
-define("CARPETA_ARCHIVO", $_SERVER['DOCUMENT_ROOT']."aulafiecyft/web/uploads/");
+define("CARPETA_ARCHIVO", $_SERVER['DOCUMENT_ROOT']."gestor/web/uploads/");
 
 ?>
